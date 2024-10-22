c3307269d264fdc98e335caa47b7398d11cda0ca
foiv <t8210126@aueb.gr>
2024-10-22 22:23:34 +0300

    t

--- server.js
+++ server.js
@@ -67,9 +67,7 @@ app.use((req, res, next) => {
 
 
 
-app.listen(4000, () => {
-  console.log('Listening for GitHub Webhooks on port 4000');
-});
+
 // Middleware to add auth token
 function addAuthToken(req, res, next) {
   if (req.isAuthenticated()) {
