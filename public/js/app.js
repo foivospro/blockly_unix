127158045b91b1931020990c8f3732fdcf6eb43d
foiv <t8210126@aueb.gr>
2025-01-04 12:28:12 +0200

    Remove the alert of copy button

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 /************************************************************
- * appjs
+ * app.js
  ************************************************************/
 // Flag to track if the workspace has changed after execution
 let workspaceChangedAfterLastExecution = false;
@@ -428,7 +428,6 @@ function saveWorkspaceLocally() {
 function copyToClipboard() {
   const text = document.getElementById('resultsArea').innerText;
   navigator.clipboard.writeText(text)
-    .then(() => alert('Text copied to clipboard!'))
     .catch((err) => {
       console.error('Unable to copy:', err);
       alert('Failed to copy text.');
